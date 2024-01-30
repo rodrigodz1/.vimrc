e465c7085605e4ff8f463e888ca0438e2a1d9616
Rodrigo <spensmusic@gmail.com>
2024-01-30 20:22:25 -0300

    update dotfiles

--- nixos-configs/polybar/launch.sh
+++
@@ -1,4 +0,0 @@
-!/bin/bash
-
-killall -q polybar
-polybar rodrigo 2>&1 | tee -a /tmp/polybar.log & disown
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,16 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.lualine"
-require "user.toggleterm"
-require "user.project"
-require "user.impatient"
-require "user.indentline"
-require "user.alpha"
-require "user.whichkey"
-require "user.autocommands"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("vim-options")
+require("lazy").setup("plugins")
+
